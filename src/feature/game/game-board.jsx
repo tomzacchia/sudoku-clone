5e6610c48fbf8497c06eb2f64d7508ece50775d0
tomzacchia <tomzacchia@live.com>
2022-01-27 16:23:12 -0800

    Add subgrid division lines

--- src/feature/game/game-board.jsx
+++ src/feature/game/game-board.jsx
@@ -5,6 +5,8 @@ import GameCell from "./game-cell";
 import { DUMMY_BOARD, BOARD_LENGTH } from "constants";
 import Cell from "models/cell.model";
 
+import styles from "./game-board.module.css";
+
 function boardReducer(state, action) {
   switch (action.type) {
     case "":
@@ -92,9 +94,38 @@ function GameBoard(props) {
     });
   }
 
+  function makeCellsGroupedBySubgrids() {
+    const subgrids = createEmptySubgridArrays(); // [[]...[]]
+
+    gameState.forEach((row, coordX) => {
+      const rowOffset = calculateOffset(coordX);
+      row.forEach((cellConfig, coordY) => {
+        const columnOffset = calculateColumnOffset(coordY);
+        const subgridIndex = rowOffset + columnOffset;
+
+        subgrids[subgridIndex].push(
+          <GameCell
+            key={`${coordX} ${coordY}`}
+            cellConfig={cellConfig}
+            coordX={coordX}
+            coordY={coordY}
+            onChangeHandler={changeHandler}
+            onKeyDownHandler={keydownHandler}
+            onClickHandler={clickHandler}
+          />
+        );
+      });
+    });
+
+    return subgrids.map((subgridContent, index) => (
+      <Grid item xs={3} key={`subgrid_${index}`}>
+        {subgridContent}
+      </Grid>
+    ));
+  }
+
   return (
-    // TODO: add constant for # of columns
-    <Grid container columns={9}>
+    <Grid container columns={BOARD_LENGTH} className={styles["board-root"]}>
       {gameState.map((row, coordX) => {
         return row.map((cellConfig, coordY) => (
           <GameCell
@@ -108,11 +139,35 @@ function GameBoard(props) {
           />
         ));
       })}
+
+      {/* 3x3 GRID DIVISION LINES */}
+      <div
+        className={`${styles["base-bar"]} ${styles["horizontal-base"]} ${styles["horizontal-1"]}`}
+      ></div>
+      <div
+        className={`${styles["base-bar"]} ${styles["horizontal-base"]} ${styles["horizontal-2"]}`}
+      ></div>
+      <div
+        className={`${styles["base-bar"]} ${styles["vertical-base"]} ${styles["vertical-1"]}`}
+      ></div>
+      <div
+        className={`${styles["base-bar"]} ${styles["vertical-base"]} ${styles["vertical-2"]}`}
+      ></div>
     </Grid>
   );
 }
 
 export default GameBoard;
+
+function createEmptySubgridArrays() {
+  var tempArray = [];
+
+  for (let i = 0; i < BOARD_LENGTH; i++) {
+    tempArray.push([]);
+  }
+
+  return _.cloneDeep(tempArray);
+}
 
 function initializeState() {
   return DUMMY_BOARD.map((row) => row.map((value) => new Cell(value)));
@@ -167,6 +222,20 @@ function calculateOffset(num) {
     offset = 3;
   } else {
     offset = 6;
+  }
+
+  return offset;
+}
+
+function calculateColumnOffset(num) {
+  let offset = 0;
+
+  if (num < 3) {
+    offset = 0;
+  } else if (num < 6) {
+    offset = 1;
+  } else {
+    offset = 2;
   }
 
   return offset;
