758baf4544a91a7cc496932cdf6ce50ab4016086
tomzacchia <tomzacchia@live.com>
2022-01-27 16:31:22 -0800

    Fix bar alignments

--- src/feature/game/game-board.jsx
+++ src/feature/game/game-board.jsx
@@ -1,21 +1,11 @@
 import { Grid } from "@mui/material";
-import React, { useReducer, useState } from "react";
+import React, { useState } from "react";
 import _ from "lodash";
 import GameCell from "./game-cell";
 import { DUMMY_BOARD, BOARD_LENGTH } from "constants";
 import Cell from "models/cell.model";
 
 import styles from "./game-board.module.css";
-
-function boardReducer(state, action) {
-  switch (action.type) {
-    case "":
-      break;
-
-    default:
-      break;
-  }
-}
 
 /*
   boardState : {
@@ -35,7 +25,6 @@ function GameBoard(props) {
    */
   function changeHandler(event, [coordX, coordY]) {
     setGameState((prevState) => {
-      // make deep copy since properties are deeply nested
       const prevStateCopy = _.cloneDeep(prevState);
 
       let userInput = _.flow(
@@ -94,36 +83,6 @@ function GameBoard(props) {
     });
   }
 
-  function makeCellsGroupedBySubgrids() {
-    const subgrids = createEmptySubgridArrays(); // [[]...[]]
-
-    gameState.forEach((row, coordX) => {
-      const rowOffset = calculateOffset(coordX);
-      row.forEach((cellConfig, coordY) => {
-        const columnOffset = calculateColumnOffset(coordY);
-        const subgridIndex = rowOffset + columnOffset;
-
-        subgrids[subgridIndex].push(
-          <GameCell
-            key={`${coordX} ${coordY}`}
-            cellConfig={cellConfig}
-            coordX={coordX}
-            coordY={coordY}
-            onChangeHandler={changeHandler}
-            onKeyDownHandler={keydownHandler}
-            onClickHandler={clickHandler}
-          />
-        );
-      });
-    });
-
-    return subgrids.map((subgridContent, index) => (
-      <Grid item xs={3} key={`subgrid_${index}`}>
-        {subgridContent}
-      </Grid>
-    ));
-  }
-
   return (
     <Grid container columns={BOARD_LENGTH} className={styles["board-root"]}>
       {gameState.map((row, coordX) => {
@@ -158,16 +117,6 @@ function GameBoard(props) {
 }
 
 export default GameBoard;
-
-function createEmptySubgridArrays() {
-  var tempArray = [];
-
-  for (let i = 0; i < BOARD_LENGTH; i++) {
-    tempArray.push([]);
-  }
-
-  return _.cloneDeep(tempArray);
-}
 
 function initializeState() {
   return DUMMY_BOARD.map((row) => row.map((value) => new Cell(value)));
@@ -222,20 +171,6 @@ function calculateOffset(num) {
     offset = 3;
   } else {
     offset = 6;
-  }
-
-  return offset;
-}
-
-function calculateColumnOffset(num) {
-  let offset = 0;
-
-  if (num < 3) {
-    offset = 0;
-  } else if (num < 6) {
-    offset = 1;
-  } else {
-    offset = 2;
   }
 
   return offset;
