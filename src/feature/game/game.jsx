9ffe8c697209a181d25efd1765fb389a4c3948aa
tomzacchia <tomzacchia@live.com>
2022-01-30 18:47:10 -0800

    Add difficulty header

--- src/feature/game/game.jsx
+++ src/feature/game/game.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import GameBoard from "./game-board";
-import { Grid, StyledEngineProvider } from "@mui/material";
+import { Card, Grid, StyledEngineProvider, Typography } from "@mui/material";
 import FinishMessage from "./finish-message";
 
 function Game(props) {
@@ -15,10 +15,21 @@ function Game(props) {
       <Grid
         container
         justifyContent="center"
+        flexDirection="column"
         spacing={3}
         style={{ height: "100vh" }}
       >
-        <Grid item xs={5} sx={{ m: "auto", height: "540px" }}>
+        <Grid
+          item
+          xs={5}
+          sx={{ m: "auto" }}
+          style={{ minHeight: "540px", minWidth: "590px" }}
+        >
+          <Card>
+            <Typography variant="h5" sx={{ mb: 2, fontWeight: "bold" }}>
+              Difficulty: Easy
+            </Typography>
+          </Card>
           {!isGameDone && <GameBoard handelGameDone={handleGameDone} />}
           {isGameDone && <FinishMessage />}
           {/* <FinishMessage /> */}
