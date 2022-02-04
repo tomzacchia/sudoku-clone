169a1b62792bbef1c640098f988e1f6287ecd69d
tomzacchia <tomzacchia@live.com>
2022-02-03 20:30:37 -0800

    Add useGetBoardByDifficulty hook

---
+++ src/hooks/game-data.hooks.js
@@ -0,0 +1,44 @@
+import { useState, useCallback } from "react";
+import { sugokuBoardURI } from "utilities/network";
+
+/**
+ *
+ * @param {*} params | {difficulty: "easy" || "medium || "hard"}
+ */
+export async function getBoardByDifficulty(params) {
+  const url = sugokuBoardURI(params);
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("Request was unsuccessful");
+  }
+
+  const data = await response.json();
+
+  return data.board;
+}
+
+export function useGetBoardByDifficulty() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [board, setBoard] = useState(null);
+
+  async function execute(options = {}) {
+    setIsLoading(true);
+    try {
+      const board = await getBoardByDifficulty(options);
+      console.log(board);
+      setBoard(board);
+    } catch (error) {
+      setError(error);
+    }
+    setIsLoading(false);
+  }
+
+  return {
+    isLoading,
+    error,
+    board,
+    execute: useCallback(execute, []),
+  };
+}
